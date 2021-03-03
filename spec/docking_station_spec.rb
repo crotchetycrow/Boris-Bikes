8997bf15f4ba3266f8f62ed9a4d031102bf119ba
crotchetycrow <jackleighton@live.co.uk>
2021-03-03 17:29:23 +0000

    Step 10 completed, made the docking stations release a bike object

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,12 @@
 require 'docking_station'
+require 'bike'
+
+test = DockingStation.new
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it 'release_bike' do
+    dock = DockingStation.new
+    expect(dock.release_bike).to respond_to :working?
+  end
 end
